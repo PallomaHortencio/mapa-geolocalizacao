e05c86da26fae4903fa1c8749c5d88bd20ee4b37
Palloma Hortencio <palloma.hortencio@gmail.com>
2023-01-19 11:09:22 -0300

    att

--- App.js
+++ App.js
@@ -4,24 +4,20 @@ import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
   const regiaoInicial = {
+    // Estado de SP
     latitude: -23.533773,
     longitude: -46.65529,
-    // latitudeDelta: 0.0922,
-    // longitudeDelta: 0.0421
     latitudeDelta: 10,
     longitudeDelta: 10,
   };
 
-  const [localizacao, setLocalizacao] = useState({
-    latitude: 26.357896,
-    longitude: 127.783809,
-    latitudeDelta: 0,
-    longitudeDelta: 0,
-  });
+  /* Usando state para controlar a localização */
+  const [localizacao, setLocalizacao] = useState();
 
   const marcarLocal = (event) => {
     setLocalizacao({
-      ...localizacao,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
       latitude: event.nativeEvent.coordinate.latitude,
       longitude: event.nativeEvent.coordinate.longitude,
     });
@@ -36,21 +32,15 @@ export default function App() {
           onPress={marcarLocal}
           style={estilos.mapa}
           region={localizacao ?? regiaoInicial}
-          liteMode={false} // somente android
-          mapType="hybrid"
-          userInterfaceStyle="light" // somente ios
-          maxZoomLevel={20}
-          minZoomLevel={2}
+          liteMode={false}
+          mapType="standard"
         >
           {localizacao && (
             <Marker
-              draggable
               coordinate={localizacao}
-              title="oie"
+              title="Aqui!!!"
               onPress={(e) => console.log(e.nativeEvent)}
-            >
-              <Image source={require("./assets/ghost.png")} />
-            </Marker>
+            />
           )}
         </MapView>
       </View>
@@ -59,11 +49,11 @@ export default function App() {
 }
 
 const estilos = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   mapa: {
     width: "100%",
     height: "100%",
+  },
+  container: {
+    flex: 1,
   },
 });
